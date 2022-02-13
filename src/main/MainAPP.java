acae6594676965a9b3150de1fee0f25e9c0e7806
AlbertNoMes <albertnotariomestres@gmail.com>
2022-02-13 18:11:25 +0100

    10.2 en el main

--- src/main/MainAPP.java
+++ src/main/MainAPP.java
@@ -1,10 +1,21 @@
+/* AUTOR: ALBERT NOTARIO MESTRES
+* DATA: 7/02/22
+*/
+
 package main;
 
 public class MainAPP {
 
 	public static void main(String[] args) {
 
-
+		try {
+			System.out.println("Mensaje mostrado por pantalla");
+			Exception e = new Exception("Esto es un objeto Exception");
+			throw e;
+		} catch (Exception ex) {
+			System.out.println("Excepcion capturada con mensaje: " + ex.getMessage());
+		}
+		System.out.println("Programa terminado");
 	}
 
 }
